aad4eb3076e47d619544c7cb9226a264130c5a0a
Happy2018new <chenbo79800@163.com>
2025-04-21 13:07:55 +0800

    minor changes

--- python/internal/symbol_export.py
+++ python/internal/symbol_export.py
@@ -1,34 +1,9 @@
-from io import BytesIO
-from ..utils import marshalNBT, unmarshalNBT
 import ctypes, struct, nbtlib
-
-
-LIB = ctypes.cdll.LoadLibrary("python/out.dll")
-
-CPtr = ctypes.c_void_p
-CSlice = CPtr
-CString = ctypes.c_char_p
-CInt = ctypes.c_int
-CLongLong = ctypes.c_longlong
-
-
-def as_c_bytes(b: bytes) -> ctypes.c_char_p:
-    return ctypes.c_char_p(struct.pack("<I", len(b)) + b)
-
-
-def as_python_bytes(slice: CSlice) -> bytes:
-    l = struct.unpack("<I", ctypes.string_at(slice, 4))[0]
-    return ctypes.string_at(slice, 4 + l)[4:]
-
-
-def as_c_string(string: str) -> CString:
-    return CString(bytes(string, encoding="utf-8"))
-
-
-def as_python_string(c_string: bytes) -> str:
-    if c_string is None:
-        return ""
-    return c_string.decode(encoding="utf-8")
+from io import BytesIO
+from .types import LIB
+from .types import CPtr, CSlice, CString, CInt
+from .types import as_c_bytes, as_python_bytes, as_c_string, as_python_string
+from ..utils import marshalNBT, unmarshalNBT
 
 
 LIB.FreeMemory.argtypes = [CPtr]
