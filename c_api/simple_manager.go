6a725052bd29dd8067f0c22315c195d628893a85
Happy2018new <chenbo79800@163.com>
2025-06-10 21:28:32 +0800

    Use runtime.pinner for simple manager

--- c_api/simple_manager.go
+++ c_api/simple_manager.go
@@ -1,23 +1,47 @@
 package main
 
 import (
-	"runtime/cgo"
+	"runtime"
+	"sync"
+	"unsafe"
 )
 
-type SimpleManager[T any] struct{}
-
-func NewSimpleManager[T any]() SimpleManager[T] {
-	return SimpleManager[T]{}
+type SimpleManager[T any] struct {
+	mapping sync.Map // map[uintptr]*runtime.Pinner
 }
 
-func (s SimpleManager[T]) AddObject(t T) int {
-	return int(cgo.NewHandle(&t))
+func NewSimpleManager[T any]() *SimpleManager[T] {
+	return &SimpleManager[T]{
+		mapping: sync.Map{},
+	}
 }
 
-func (s SimpleManager[T]) LoadObject(id int) *T {
-	return cgo.Handle(id).Value().(*T)
+func (s *SimpleManager[T]) AddObject(t T) int {
+	goPtr := &t
+
+	pinner := new(runtime.Pinner)
+	pinner.Pin(goPtr)
+
+	ptr := uintptr(unsafe.Pointer(goPtr))
+	s.mapping.Store(ptr, &pinner)
+
+	return int(ptr)
 }
 
-func (s SimpleManager[T]) ReleaseObject(id int) {
-	cgo.Handle(id).Delete()
+func (s *SimpleManager[T]) LoadObject(ptr int) *T {
+	return (*T)(unsafe.Pointer(uintptr(ptr)))
+}
+
+func (s *SimpleManager[T]) ReleaseObject(ptr int) {
+	value, ok := s.mapping.LoadAndDelete(uintptr(ptr))
+	if !ok {
+		return
+	}
+
+	pinner, ok := value.(*runtime.Pinner)
+	if !ok {
+		return
+	}
+
+	pinner.Unpin()
 }
