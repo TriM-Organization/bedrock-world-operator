210d4a5e7b421dc07130022aa4300e30f30ad48f
Happy2018new <chenbo79800@163.com>
2025-06-10 20:37:07 +0800

    Use cgo.handle for simple manager

--- c_api/simple_manager.go
+++ c_api/simple_manager.go
@@ -1,43 +1,23 @@
 package main
 
 import (
-	"maps"
-	"sync"
+	"runtime/cgo"
 )
 
-type SimpleManager[T any] struct {
-	mapping map[int]*T
-	mu      *sync.RWMutex
-	ptr     int
+type SimpleManager[T any] struct{}
+
+func NewSimpleManager[T any]() SimpleManager[T] {
+	return SimpleManager[T]{}
 }
 
-func NewSimpleManager[T any]() *SimpleManager[T] {
-	return &SimpleManager[T]{
-		mapping: make(map[int]*T),
-		mu:      new(sync.RWMutex),
-		ptr:     -1,
-	}
+func (s SimpleManager[T]) AddObject(t T) int {
+	return int(cgo.NewHandle(&t))
 }
 
-func (s *SimpleManager[T]) AddObject(t T) int {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.ptr++
-	s.mapping[s.ptr] = &t
-	return s.ptr
+func (s SimpleManager[T]) LoadObject(id int) *T {
+	return cgo.Handle(id).Value().(*T)
 }
 
-func (s *SimpleManager[T]) LoadObject(id int) *T {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	return s.mapping[id]
-}
-
-func (s *SimpleManager[T]) ReleaseObject(id int) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	delete(s.mapping, id)
-	newMapping := make(map[int]*T)
-	maps.Copy(newMapping, s.mapping)
-	s.mapping = newMapping
+func (s SimpleManager[T]) ReleaseObject(id int) {
+	cgo.Handle(id).Delete()
 }
